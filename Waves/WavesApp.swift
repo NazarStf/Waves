2959518879c61c9f82a3b0bb6a1b23f4458ea972
NazarStf <hworker98@gmail.com>
2023-07-29 18:46:40 +0300

    Initial Commit

---
+++ Waves/WavesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WavesApp.swift
+//  Waves
+//
+//  Created by NazarStf on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct WavesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
